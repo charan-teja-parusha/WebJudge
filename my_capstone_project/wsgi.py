b28f450c4bcff6b7aa32c8aa42b7fe5e11787edd
Charan Teja Parusha <81465996+charan-teja-parusha@users.noreply.github.com>
2023-04-02 10:10:10 -0400

    Update wsgi.py

--- my_capstone_project/wsgi.py
+++ my_capstone_project/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 import sys
-sys.path.append('/my_capstone_project/env/')
+sys.path.append('/home/site/wwwroot/my_capstone_project/env')
 
 from django.core.wsgi import get_wsgi_application
 
