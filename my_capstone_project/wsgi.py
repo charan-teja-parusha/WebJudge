f1f34c7d80b1c96af1e0b3d9d7edf0bf9071ffc7
Charan Teja Parusha <81465996+charan-teja-parusha@users.noreply.github.com>
2023-04-02 08:29:52 -0400

    Update wsgi.py

--- my_capstone_project/wsgi.py
+++ my_capstone_project/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+import sys
+sys.path.append('/my_capstone_project/env/')
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_capstone_project.settings')
